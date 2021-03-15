c9036892baeba13c001024b0094b0ba6491017af
AlexSaplacan <a.saplacan@livelinktechnology.net>
2021-03-15 22:11:09 +0000

    upgrade to 2.92

--- display_used_indexes.py
+++ display_used_indexes.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Used Indexes",
     "author": "Alex Saplacan",
-    "version": (0, 3),
-    "blender": (2, 78, 0),
+    "version": (0, 4),
+    "blender": (2, 90, 0),
     "location": "Properties",
     "description": "Displays the Material or object indexes already used.",
     "warning": "",
@@ -10,53 +10,85 @@ bl_info = {
     "category": "UI",
     }
 
+
 import bpy
+
 
 def draw_indexes(self, context, idx_list):
 
-    draw_text = str(idx_list[0])
-    for ind in idx_list[1:]:
-        draw_text += ', %s' %(ind)
+    draw_text = " ".join(str(i) for i in idx_list)
 
     layout = self.layout
-    col = layout.column()
-    col.label("Indexes already used:")
-    col.label(draw_text)
+    flow = layout.grid_flow(
+        row_major=True,
+        columns=0,
+        even_columns=True,
+        even_rows=False,
+        align=False
+    )
+    col = flow.column()
+    col.separator()
+    col.label(text="Indexes already used:")
+    col.label(text=draw_text)
+
+
+def get_scene_obj_indexes(context):
+    objects = context.scene.objects
+    ob_used_idx = {obj.pass_index for obj in objects}
+    return ob_used_idx
+
+
+def get_mat_indexes():
+    materials = bpy.data.materials
+    mat_used_idx = {mat.pass_index for mat in materials}
+    return mat_used_idx
 
 
 def draw_obj_used_indexes(self, context):
     """
     Display the Object IDs already used
     """
-    scene = context.scene
+    idxs = get_scene_obj_indexes(bpy.context)
+    draw_indexes(self, context, idxs)
 
-    ob_indx_list = []
-    for obj in bpy.context.scene.objects:
-        idx = obj.pass_index
-        if idx not in ob_indx_list:
-            ob_indx_list.append(idx)
-    ob_indx_list.sort()
-    draw_indexes(self, context, ob_indx_list)
 
 def draw_mat_used_indexes(self, context):
     """
     Display IDs already used by other materials.
     """
-    mat_indx_list = []
-    for material in bpy.data.materials:
-        idx = material.pass_index
-        if idx not in mat_indx_list:
-            mat_indx_list.append(idx)
-    mat_indx_list.sort()
-    draw_indexes(self, context, mat_indx_list)
+    mat_used_idx = get_mat_indexes()
+    draw_indexes(self, context, mat_used_idx)
+
+
+def test_get_scene_obj_idx_returns_expected():
+    # GIVEN a object with object index 2
+    cube = bpy.context.scene.objects['Cube']
+    cube.pass_index = 2
+    # WHEN get_scene_obj_indexes is ran
+    result = get_scene_obj_indexes(bpy.context)
+    # THEN it returns the exepected result
+    assert result == {0, 2}
+
+
+def test_get_mat_obj_idx_returns_expected():
+    # GIVEN a material with an index 2
+    mat = bpy.data.materials['Material']
+    mat.pass_index = 5
+    # WHEN get_mat_indexes is called
+    result = get_mat_indexes()
+    # THEN it returns the exepected result 
+    assert result == {0, 5}
+
 
 def register():
     bpy.types.OBJECT_PT_relations.append(draw_obj_used_indexes)
-    bpy.types.CyclesMaterial_PT_settings.append(draw_mat_used_indexes)
+    bpy.types.EEVEE_MATERIAL_PT_settings.append(draw_mat_used_indexes)
+
 
 def unregister():
     bpy.types.OBJECT_PT_relations.remove(draw_obj_used_indexes)
-    bpy.types.CyclesMaterial_PT_settings.remove(draw_mat_used_indexes)
+    bpy.types.EEVEE_MATERIAL_PT_settings.remove(draw_mat_used_indexes)
+
 
 if __name__ == "__main__":
     register()
